b0ab2687e212ff6b9cf84c97beea1eb9df0fa847
yucellsarikaya <2017141050@cumhuriyet.edu.tr>
2021-08-02 16:54:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import MainContext from './MainContext';
+import Sidebar from './components/Sidebar'
+import Content from './components/Content';
+import BrandsData from './brands.json';
+import { useEffect, useState } from 'react';
+import Copied from './components/Copied';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Collection from './components/Collection';
+import {forceCheck}  from "react-lazyload";
+
+
+function App() {
+  const brandsArray = []
+  Object.keys(BrandsData).map(key => { //jsonda ki verileri arraya aktarildi
+    brandsArray.push(BrandsData[key])
+  })
+  const [brands, setBrands] = useState(brandsArray)
+  const [selectedBrands, setSelectedBrands] = useState([])
+  const [copied, setCopied] = useState(false)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log(selectedBrands)
+  }, [selectedBrands])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false)
+    }, 500)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [copied])
+
+  useEffect(() => { //input da her degisen veriyi json da arar
+    setBrands(brandsArray.filter(brand => brand.title.toLowerCase().includes(search)))
+    forceCheck()
+  }, [search])
+
+  const data = {
+    brands,
+    selectedBrands,
+    setSelectedBrands,
+    setCopied,
+    search,
+    setSearch
+  }
+
+  return (
+    <>
+      <MainContext.Provider value={data}>
+        {copied && <Copied color={copied} />}
+        <Sidebar />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Content />
+            </Route>
+            <Route path="/collection/:slugs">
+              <Collection />
+            </Route>
+          </Switch>
+        </Router>
+      </MainContext.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Download.js
@@ -0,0 +1,75 @@
+import { useContext, useEffect, useState } from 'react'
+import MainContext from '../MainContext';
+import { GrLink, GrDownload, GrClose } from 'react-icons/gr';
+import {Link} from 'react-router-dom'
+
+function Download() {
+	const { selectedBrands, setSelectedBrands, brands } = useContext(MainContext)
+	const [downloadUrl, setDownloadUrl] = useState()
+	const [cssMethod, setCssMethod] = useState('css')
+
+	useEffect(() => {
+		if (selectedBrands.length > 0) {
+			let output = ''
+			switch (cssMethod) {
+				case 'css':
+					output += ':root {\n'
+					selectedBrands.map(slug => {
+						let brand = brands.find(brand => brand.slug === slug)
+						brand.colors.map((color, key) => {
+							output += `--${slug}-${key}: #${color};\n`
+						})
+					})
+					output += '}'
+					break;
+				case 'scss':
+					selectedBrands.map(slug => {
+						let brand = brands.find(brand => brand.slug === slug)
+						brand.colors.map((color, key) => {
+							output += `\$${slug}-${key}: #${color};\n`
+						})
+					})
+					break;
+				case 'less':
+					selectedBrands.map(slug => {
+						let brand = brands.find(brand => brand.slug === slug)
+						brand.colors.map((color, key) => {
+							output += `@${slug}-${key}: #${color};\n`
+						})
+					})
+					break;
+			}
+			const blob = new Blob([output])
+			const url = URL.createObjectURL(blob)
+			setDownloadUrl(url)
+			return () => {
+				URL.revokeObjectURL(url)
+				setDownloadUrl('')
+			}
+		}
+	}, [selectedBrands, cssMethod])
+
+	return (
+		<div className="download">
+			<div className="actions">
+				<a download={`brands.${cssMethod}`} href={downloadUrl}>
+					<GrDownload />
+				</a>
+				<select onChange={(e) => setCssMethod(e.target.value)}>
+					<option value="css">CSS</option>
+					<option value="scss">SCSS</option>
+					<option value="less">LESS</option>
+				</select>
+				<Link to={`/collection/${selectedBrands.join(',')}`}>
+					<GrLink />
+				</Link>
+			</div>
+			<div className="selected" onClick={() => setSelectedBrands([])}>
+				<GrClose />
+				{selectedBrands.length} brands collected
+			</div>
+		</div>
+	)
+}
+
+export default Download
